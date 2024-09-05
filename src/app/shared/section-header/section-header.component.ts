1f170b5af8fc85e9797b31eaf0492fcb6688ed9a
Ross Bryson <jacobrbryson@gmail.com>
2024-09-05 06:48:59 -0500

    OG-14: Fixed iOS styling issues

--- src/app/shared/section-header/section-header.component.ts
+++ src/app/shared/section-header/section-header.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'shared-section-header',
   templateUrl: './section-header.component.html',
+  styleUrls: ['./section-header.component.scss'],
 })
 export class SectionHeaderComponent {
   @Input() title: string | undefined;
